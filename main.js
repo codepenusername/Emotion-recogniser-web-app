5cc312454896b1366e8a0f2402d7bdfaebf5b7e1
Ayush <100988794+codepenusername@users.noreply.github.com>
2023-04-29 15:59:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function modelLoaded()
+{
+    console.log("Model is ready!!!")
+}
+Webcam.set
+    ({
+        width:350,
+        height:300,
+        image_format : 'png',
+        png_quality:90
+    });
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function (data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/h4ew3oqbQ/model.json', modelLoaded);
+
+function check()
+{ 
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results)
+{ 
+    if (error)
+    { 
+        console.log(error);
+    }
+    else
+    { 
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
